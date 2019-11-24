f49dbe60f83366d3b4d054098f1a5bf5e1a45a1e
Nick <pattison.nick@gmail.com>
2019-11-23 22:57:00 -0500

    Added project working files

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelInfo {
+    public string nameOfLevel = "";
+    public string nameOfCreator = "";
+    public string filenameOfScene = "";
+    public Sprite imageForButton;
+}
+
+public class Levels : MonoBehaviour
+{
+    public LevelInfo[] levels;
+}
